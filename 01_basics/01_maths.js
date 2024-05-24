3792dfce381435d859fa4516a799f8b17aa22e08
chotu <113451307+01Chhotu@users.noreply.github.com>
2024-05-24 17:26:08 +0000

    add maths

---
+++ 01_basics/01_maths.js
@@ -0,0 +1,8 @@
+// random gives value 0 t0 1
+// console.log(Math.random())
+// console.log(Math.floor((Math.random()*10)+1))
+
+// if requre number between min-10 to max-20
+const min=10
+const max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min))
