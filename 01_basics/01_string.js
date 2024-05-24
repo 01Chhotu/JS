08a0ca454cf4bd8466b31cd3045cd69903d322a4
chotu <113451307+01Chhotu@users.noreply.github.com>
2024-05-24 17:03:16 +0000

    add string

---
+++ 01_basics/01_string.js
@@ -0,0 +1,19 @@
+let name="chhotu"
+let repo = 26;
+
+// console.log(`Hello my name is ${name} paswan ans my repo count is ${repo}`)
+const gameName = new String('chhotu-Paswan-com')
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('P'))
+const newString = gameName.substring(0, 4)
+// console.log(newString)
+const anotherString = gameName.slice(-10, 4)
+// console.log(anotherString)
+const trimString = "     chhotu   "
+// console.log(trimString)
+// console.log(trimString.trim())
+const url = "https:/chhotu.com/chhotu%20paswan"
+// console.log(url.replace('%20', '-'))
+// console.log(gameName.split('-'))
