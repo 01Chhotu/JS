71ebe64f8ef648b95ce4125a356f1898d731e3bb
chotu <113451307+01Chhotu@users.noreply.github.com>
2024-06-01 21:10:24 +0000

    add BmiCalculator project

---
+++ 09_project/02_BMICalculator/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form')
+// console.log(form)
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if(height==='' || height<=0 || isNaN(height)){
+        results.innerHTML = "Plase given valid height";
+    }
+    else if(weight==='' || weight<=0 || isNaN(weight)){
+        results.innerHTML = "Plase given valid weight";
+    }
+    else{
+        const guide = document.querySelector('#weight-guide');
+        // console.log(guide.children)
+        const BMI = (weight/((height*height)/10000)).toFixed(2);
+        results.innerHTML = `<span>your BMI Index is ${BMI}</span>`;
+        if(BMI<=18.6){
+            guide.children[1].style.color = "red"
+        }
+        else if(BMI>24.9){
+            guide.children[3].style.color = "red"
+        }
+        else{
+            guide.children[2].style.color = "green"
+        }
+
+    }
+
+})
